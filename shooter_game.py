04665501a018afecb7b4a1597115e41cc4ef7d72
fbhgjuqahghgehwaukwrg <94283918+fbhgjuqahghgehwaukwrg@users.noreply.github.com>
2021-11-20 21:26:43 +0300

    Update shooter_game.py
    Сделал управление

--- shooter_game.py
+++ shooter_game.py
@@ -1,11 +1,9 @@
 from pygame import *
-from random import *
-score = 0
-lost = 0
+from random import randint
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size, size1 ):
+    def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size, size1))
+        self.image = image.load(player_image)
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -15,40 +13,35 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 0:
-                self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 650:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top, 10, 10, 30)
-        bullets.add(bullet)
-class Enemy(GameSprite):
-    def update(self):
-        self.rect.y += self.speed
-        if self.rect.y > 525:
-            self.rect.y = 0
-            self.rect.x = randint(5, 700)
-            global lost
-            lost += 1
-class Bullet(GameSprite):
-    def update(self):
-        if self.rect.y > 0:
+        if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        else:
-            self.kill()
-monsters = sprite.Group()
-bullets = sprite.Group()
-for i in range(4):
-    monster = Enemy("asteroid.png", randint(0, 650), -10, randint(1,2), 50, 50)
-    monsters.add(monster)
+        if keys[K_s]:
+            if self.rect.y < height - 148:
+                self.rect.y += self.speed
+            else:
+                self.rect.y = height - 148
+    def update1(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+                self.rect.y -= self.speed
+        if keys[K_DOWN]:
+            if self.rect.y < height - 148:
+                self.rect.y += self.speed
+            else:
+                self.rect.y = height - 148
+       
+init()
+width = int(display.Info().current_w)
+height = int(display.Info().current_h)
 clock = time.Clock()
-
 FPS = 60
-window = display.set_mode((700, 500))
-display.set_caption("Шутер")
-background = transform.scale(image.load("galaxy.jpg"), (700, 500))
-player = Player("rocket.png", 350, 400, 10, 50, 100)
+#window = display.set_mode((700, 500))
+window = display.set_mode()
+#display.set_caption("Пинг-понг")
+background = transform.scale(image.load("background.bmp"), (width, height))
 game = True
+player1 = Player("player.bmp", 10, width / 2, 10)
+player2 = Player("player.bmp", width - 25, width / 2, 10)
 font.init()
 font = font.SysFont("Arial", 25)
 while game:
@@ -56,24 +49,9 @@ while game:
     for i in event.get():
         if i.type == QUIT:
             game = False
-        elif i.type == KEYDOWN:
-            if i.key == K_SPACE:
-                player.fire()
-    collides = sprite.groupcollide(monsters, bullets, True, True)
-    for c in collides:
-        score += 1
-        monster = Enemy("asteroid.png", randint(0, 650), -10, randint(1, 2), 50, 50)
-        monsters.add(monster)
-    sprite_lost = font.render("Пропущено: " + str(lost), True, (255, 255, 255))
-    sprite_num = font.render("Сбито: " + str(score), True, (255, 255, 255))
-    window.blit(sprite_lost, (5, 5))
-    window.blit(sprite_num, (5, 30))
-    monsters.draw(window)
-    monsters.update()
-    bullets.draw(window)
-    bullets.update() 
-    monsters.update()
-    player.reset() 
-    player.update()     
+    player1.reset()
+    player1.update()
+    player2.reset()
+    player2.update1()
     clock.tick(FPS)    
     display.update()
